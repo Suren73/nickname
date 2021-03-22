ca9d2759c6121eee3d544fb2220b40f3fae5316d
Andrei Surkov <andronsurkov73@gmail.com>
2021-03-22 18:51:07 +0300

    Добавил выполненное Домашнее задание

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
+export default class Validator {
+  constructor(name) {
+    this.name = name;
+  }
 
+  validateUsername() {
+    return !/(\d+){4,}/gi.test(this.name) && /^[a-z][a-z_-\d]*[a-z]$/gi.test(this.name);
+  }
+}
